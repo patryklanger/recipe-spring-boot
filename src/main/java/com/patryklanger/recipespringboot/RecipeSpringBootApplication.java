66d506faddfffecf5e1ed99423e5081ddb7389ce
Patryk Langer <patryklanger@icloud.com>
2021-12-28 16:51:40 +0100

    init

---
+++ src/main/java/com/patryklanger/recipespringboot/RecipeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.patryklanger.recipespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patryklanger/recipespringboot/RecipeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patryklanger.recipespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
